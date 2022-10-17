be329fb75bea03be8bef130deaea1d1706b9afc8
popyson1648 <setoshun00424@gmail.com>
2022-10-17 23:31:52 +0900

    editting cmdLineInspection()

--- PerpetualCalendar.java
+++ PerpetualCalendar.java
@@ -4,63 +4,87 @@ import java.lang.*;
 
 public class PerpetualCalendar {
 
+// Constants for extracting cmd line input values
+// The format is such an array => {000, 00, 0000}
+final static int CMD_START = 1;
+final static int CMD_END = 4;
+final static int MONTH_START = 6;
+final static int MONTH_END = 8;
+final static int YEAR_START = 10;
+final static int YEAR_END = 14;
+
+// command type
+final static String CMD = "command";
+final static String MONTH = "month";
+final static String YEAR = "year";
+
   public static void main(String[] args) {
     Calendar calendar = Calendar.getInstance();
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-    String commandLine = "";
-    String command = "";
-    byte month = 0;
-    byte year = 0;
-    String[] commandLineSplit = new String[15];
+    String cmdLine = "";
+    String cmd = "";
+    int month = 0;
+    int year = 0;
+    boolean 
 
       // $ の出力をおこなう？
       System.out.println("説明"); // 説明を書く
       try {
-        commandLine = in.readLine();
+        cmdLine = in.readLine();
       } catch (IOException e) {
         System.out.println("エラーが発生しました。入力をやり直してください。");
       }
-      
-      // commandLine を回してcommandLineSpilitを経由し、commandに格納。
 
+      // extractingStr() を使って、コマンドの抽出を１文字ずつ行いコマンドラインの検査を行う
+      // cmdLineInspection....
 
-
-// ----------------------------------------------------------------------------------------------
-      // if (!(Objects.isNull(rowMonth))) { // 引数がある場合は、月と年の制限表示の処理に進む。
-
-      //   month = Byte.parseByte(rowMonth);
-      //   year = Byte.parseByte(rowYear);
-       
-      //   // judgiment command
-      // if (!(("cal").equals(command))) {
-      //   if (command == null){
-      //     System.out.println("コマンドを入力してください。");
-      //   }
-      //   System.out.println("コマンドに誤りがあります。実行可能なコマンドは\"cal\"です。あなたの入力したコマンドは" + command + "でした。");
-      //   continue;
-      // }
-      //   if (!(month > 1 && month < 13)) {
-      //     System.out.println("第一引数に誤りがあります。受付可能な数値は1から12です。");
-      //     continue;
-      //   }
-      //   if (!(year > 0)) {
-      //     System.out.println("第二引数に誤りがあります。受付可能な数値は1以上です。"); 
-      //     continue;
-      //   }
-
-      // }
-      
   }
 
-  // sourceArray の内容を指定した位置から位置まで workingArray に格納する関数。
-  public static void (String[] sourceArray, String[] workingArray, short splitStart, short splitEnd) {
-    for (int i=splitStart; i<splitEnd; i++) {
-      workingArray[i] = sourceArray[i];
+    // 文字列から任意の範囲の文字を取り出す関数
+    public static String extractingStr(String srcVar, int extractingStart, int extractingEnd) { // 抽出される文字 x は、extractingStart　=< x < extractingEnd
+        String x = "";
+        x = srcVar.substring(extractingStart, extractingEnd);
+        return x;
     }
-  }
 
-  // String[] workingArray を初期化する関数。
-  public static void
+    // コマンドライン入力の検閲をする関数
+    public static boolean cmdLineInspection(Strnig srcCmd, String cmdType, int cmdLen){ //cmdTypeの引数は定数command type。
+      boolean inspectionResult = false;
+      String tempSrcCmd = "";
 
-}
+      if (cmdType.equals(CMD)) {
+        String tempCmd = "cal";  
+      }
+      if (cmdType.equals(MONTH)) {
+        int limitL = 1;
+        int limitH = 12;
+      }
+      if (cmdType.equals(YEAR)) {
+        int limitL = 1;
+        int limitH = 9999;
+      }
+      
+            for (int i<1; i<cmdLine+1; i++) { // extractingStr()の仕様により+1する。
+              tempSrcCmd = extractingStr(srcCmd, 1, i);
+              tempCmd = extractingStr(cmd, 1, i);
+
+              if (!(tempSrcCmd.equals(tempCmd)) {
+                return inspectionResult;
+              }
+            }
+      inspectionResult = true;
+      return inspectionResult;
+    }
+
+    // ToDo
+
+    // - コマンドラインの引数に無効な入力を受けたときに弾く処理
+    // - - cal
+    // - - - caaaaal, cccccccal, abc
+    // - - 12
+    // - - - 111112, 122222
+    // - - 2022
+    // - - - 2222222222202, 200000022, 2022222222222222
+
+    // コマンドライン検査でcalだけでなく数値も検査できるようにする。

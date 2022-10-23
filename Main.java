9c08c0572dbfe1941ecd06ae7e2d3af8d51ba114
motoii <coma.zwei7@gmail.com>
2022-10-24 08:26:33 +0900

    Add Description of the program and the process of repeating the input

--- Main.java
+++ Main.java
@@ -7,7 +7,9 @@ public class Main {
 // E2: ArrayIndexOutOfBoundsException
 
 public static void main(String[] args) {
-
+    boolean isInitialStartup = true;
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+while (true) {
     String cmdLine = null;
 
     int argCnt = 0;
@@ -21,17 +23,32 @@ public static void main(String[] args) {
     int firstDayOfWeek = 0;
     int endDayOfMonth = 0;
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("プログラム起動の旨");
-
+    input:
     while (!(inspectionCode == 0)) {
-        System.out.println("コマンドの使い方等");
-        // $ の出力を行う？
+        if (isInitialStartup) {
+            System.out.println("PerpetualCalendarを起動しました。\n");
+            System.out.println("==========使い方==========");
+            System.out.println("受付可能な入力形式は以下です。");
+            System.out.println("$ cal [month [year]]");
+            System.out.println();
+            System.out.println("-----例-----");
+            System.out.println("$ cal          -> 現在のカレンダーが表示されます。");
+            System.out.println("$ cal 12       -> 今年の12月のカレンダーを表示します。");
+            System.out.println("$ cal 12 2020  -> 2020年12月のカレンダーを表示します。");
+            System.out.println("------------");
+            System.out.println("プログラムは q で終了します。");
+            System.out.println("==========================");
+            System.out.println();
+        }
+        System.out.print("$ ");
         try {
             cmdLine = in.readLine();
         } catch (IOException e) {
             System.out.println("エラーが発生しました。エラーコード: E1");
             continue;
+        }
+        if (Objects.requireNonNull(cmdLine).matches("q")) {
+            System.exit(0);
         }
 
         inspectionResult = InputInspection.inspection(cmdLine); // Command lineに入力された値の検査
@@ -39,17 +56,18 @@ public static void main(String[] args) {
 
         // 検査結果の検査コードを用いて判定結果を出力
         switch (inspectionCode) {
-            case 0 : break;
-            case 1 :
+            case 0:
+                break;
+            case 1:
                 System.out.println("入力したコマンドに誤りがある可能性があります。正しいコマンドは\"cal\"です。");
                 continue;
-            case 2 :
+            case 2:
                 System.out.println("入力したmonthの値に誤りがある可能性があります。");
                 continue;
-            case 3 :
+            case 3:
                 System.out.println("入力したyearの値に誤りがある可能性があります。");
                 continue;
-            case 4 :
+            case 4:
                 System.out.println("入力形式に誤りがある可能性があります。");
         }
     }
@@ -68,24 +86,23 @@ public static void main(String[] args) {
         // 引数入力がない場合は、現在のmonth, year, yearの桁数を各変数セット。FormatCalendarクラスのフォーマット関数群に使用する。
         // 引数が1(monthだけ)の場合、year, yearの桁数を各変数セット。
         switch (argCnt) {
-            case 0 :
-                month = (calendar.get(Calendar.MONTH)) +1 ; // CalendarクラスのMONTHフィールドは 0 が JANUARY であるため、+1する。Calendarクラスで使用する値ではなく、そのままmonthとして利用するためである。例えば今月が10月なら、取得される値は9のため+1して10にする。
+            case 0:
+                month = (calendar.get(Calendar.MONTH)) + 1; // CalendarクラスのMONTHフィールドは 0 が JANUARY であるため、+1する。Calendarクラスで使用する値ではなく、そのままmonthとして利用するためである。例えば今月が10月なら、取得される値は9のため+1して10にする。
                 year = calendar.get(Calendar.YEAR);
                 numOfDigitYear = String.valueOf(calendar.get(Calendar.YEAR)).length();
-            case 1 :
-                calendar.set(Calendar.MONTH, month-1); // CalendarクラスのMONTHフィールドは 0 が JANUARY であるため -1 をする。month は JANUARY を 1 としている。
+            case 1:
+                calendar.set(Calendar.MONTH, month - 1); // CalendarクラスのMONTHフィールドは 0 が JANUARY であるため -1 をする。month は JANUARY を 1 としている。
                 calendar.set(Calendar.DAY_OF_MONTH, 1);
                 year = calendar.get(Calendar.YEAR);
                 numOfDigitYear = String.valueOf(calendar.get(Calendar.YEAR)).length();
-            case 2 :
-                calendar.set(Calendar.MONTH, month-1);
+            case 2:
+                calendar.set(Calendar.MONTH, month - 1);
                 calendar.set(Calendar.YEAR, year);
                 calendar.set(Calendar.DAY_OF_MONTH, 1);
-            }
         }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("エラーが発生しました。エラーコード: E2");
-        }
+    } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("エラーが発生しました。エラーコード: E2");
+    }
 
     firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);  // 今月の最初の曜日(一日目の曜日)を取得
     endDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);  // monthの最終日を取得。
@@ -97,13 +114,17 @@ public static void main(String[] args) {
 
     // フォーマットされたカレンダーを出力。
     FormatCalendar.printCalendar(FormatCalendar.titleLine,
-                  FormatCalendar.WEEK_OF_DAY_LINE,
-                  FormatCalendar.dayOfMonthLine1,
-                  FormatCalendar.dayOfMonthLine2,
-                  FormatCalendar.dayOfMonthLine3,
-                  FormatCalendar.dayOfMonthLine4,
-                  FormatCalendar.dayOfMonthLine5,
-                  FormatCalendar.dayOfMonthLine6);
-  }
+            FormatCalendar.WEEK_OF_DAY_LINE,
+            FormatCalendar.dayOfMonthLine1,
+            FormatCalendar.dayOfMonthLine2,
+            FormatCalendar.dayOfMonthLine3,
+            FormatCalendar.dayOfMonthLine4,
+            FormatCalendar.dayOfMonthLine5,
+            FormatCalendar.dayOfMonthLine6);
+    System.out.println();
 
-  }
+    isInitialStartup = false;
+    }
+    }
+
+}
